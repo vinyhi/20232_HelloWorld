9496f1f12f503fd2f115693020e459d3d8384bcf
Vinicius Bosso <vinih_bosso@hotmail.com>
2023-08-31 16:20:25 -0300

    commited

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
+import javax.swing.JOptionPane;
 public class HelloWorld{
     public static void main(String [] args){
         //stdin stdout stderr
         System.out.println("Hello, World");
+        JOptionPane.showMessageDialog(null, "Hello, World");
     } 
 }
 //em java, o compilador produz bytecode
