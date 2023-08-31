237a7317ad8bba0cb765226d8455e14605f7f2da
Vinicius Bosso <vinih_bosso@hotmail.com>
2023-08-31 15:48:00 -0300

    ap01 hello wolrd
    -utilizand linha de comando

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String [] args){
+        //stdin stdout stderr
+        System.out.println("Hello, World");
+    } 
+}
+//em java, o compilador produz bytecode
+//é a linguagem que jvm entende
+
+//a jvm interpreta byecode
